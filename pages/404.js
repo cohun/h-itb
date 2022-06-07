71684587e0a4ff94558c0c6c55e13df3029a5911
Colin Hunwald <colin.hunwald@gmail.com>
2022-06-07 07:58:30 +0200

    Add 404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageNotFound() {
+  return (
+    <div>
+      <h2>Ez az oldal nem elérhető, menj a kezdőlapra!</h2>
+    </div>
+  );
+}
+
+export default PageNotFound;
