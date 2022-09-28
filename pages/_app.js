f6c90fc71f126e25cbed49b101a2b8e3d055852a
Colin Hunwald <colin.hunwald@gmail.com>
2022-09-28 19:52:43 +0200

    Analytics afterInteractive

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,10 @@ import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
+      {/* <Script
         id="my-script"
         strategy="lazyOnload"
-        // src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        src="https://www.googletagmanager.com/gtag/js?id=G-D8EETL2KSM"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
       <Script id="my-script" strategy="lazyOnload">
@@ -17,12 +16,27 @@ function MyApp({ Component, pageProps }) {
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-D8EETL2KSM');
-        /* gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
         page_path: window.location.pathname,
-        }); */
+        });
     `}
+      </Script> */}
+
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
       </Script>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
