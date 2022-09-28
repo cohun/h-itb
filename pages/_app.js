314e2236bf2892bf7a2d6296ccbdaccb52775939
Colin Hunwald <colin.hunwald@gmail.com>
2022-09-28 08:13:15 +0200

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,30 @@
-import Layout from '../components/Layout';
-import '../styles/globals.css';
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        id="my-script"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="my-script" strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
