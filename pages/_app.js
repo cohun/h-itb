09cb442772ca483fb8fd828899b51fb5c0f87450
Colin Hunwald <colin.hunwald@gmail.com>
2022-10-04 13:42:52 +0200

    add more search phrases

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,10 @@
-import Layout from '../components/Layout';
-import '../styles/globals.css';
-import Script from 'next/script';
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <Script
-        id="my-script"
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script id="my-script" strategy="lazyOnload">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
-      </Script> */}
-
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
         strategy="afterInteractive"
