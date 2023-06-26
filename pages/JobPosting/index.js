63c9b9c19cc2b7d77da89f9fa18f3f15b64c6020
Colin Hunwald <colin.hunwald@gmail.com>
2023-06-26 13:06:23 +0200

    Add JobPosting

--- pages/JobPosting/index.js
+++ pages/JobPosting/index.js
@@ -1,6 +1,6 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import Head from 'next/head';
+import Image from "next/image";
+import Link from "next/link";
+import Head from "next/head";
 
 const JobPosting = () => {
   return (
